d63c1102e276fd7ee7fc064b46928fd5495c00a3
Julian Blasco <blascoblasco9@gmail.com>
2020-09-23 21:13:01 -0300

    Create basic home page

---
+++ src/modules/home.js
@@ -0,0 +1,20 @@
+import navbar from './navbar';
+import footer from './footer';
+
+const home = () => {
+	const main = document.createElement('main');
+
+	const title = document.createElement('h1');
+	title.innerText = 'Restaurant Title';
+
+	const text = document.createElement('p');
+	text.innerText =
+		'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius eligendi natus sunt voluptate saepe doloribus non soluta exercitationem architecto atque!';
+
+	main.appendChild(navbar());
+  main.appendChild(title);
+  main.appendChild(text)
+	main.appendChild(footer());
+
+	return main;
+};
